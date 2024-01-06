73ce5844f521395304a50cf259fe246fa193be63
ria-hue <mumuninasaria@gmail.com>
2024-01-05 22:16:18 -0700

    added rate limiting and throttling middleware

--- src/route/noteRoute.js
+++ src/route/noteRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const controller = require("../controller/noteController");
-// const authenticateToken = require("../lib/authentication");
+const authenticateToken = require("../lib/authentication");
 const router = require("express").Router();
 
-router.post("/notes" ,controller.create);
-router.put("/notes/:id" ,controller.update);
-router.get("/notes" ,controller.notes);
-router.get("/notes/:id" ,controller.note);
-router.delete("/notes/:id" ,controller.delete);
+router.post("/notes" ,  authenticateToken ,controller.create);
+router.put("/notes/:id" ,  authenticateToken ,controller.update);
+router.get("/notes" , authenticateToken ,controller.notes);
+router.get("/notes/:id" , authenticateToken ,controller.note);
+router.delete("/notes/:id" , authenticateToken ,controller.delete);
 router.get("/search" ,controller.search);
-router.get("note/:id/shareNote" ,controller.search);
+router.get("note/:id/shareNote" , authenticateToken ,controller.search);
 
 
 module.exports = router;
